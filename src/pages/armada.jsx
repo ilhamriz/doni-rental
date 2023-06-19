50201fdbf2c0a9324ddfe1536db506684d30eabe
ilhamriz <milhamrizky7@gmail.com>
2023-06-19 22:10:05 +0700

    add armada pages

--- src/pages/armada.jsx
+++ src/pages/armada.jsx
@@ -1,25 +1,14 @@
-import Layout from '@/Layout/Layout'
-import Link from 'next/link'
-import React from 'react'
+import Layout from "@/Layout/Layout";
+import { Armada, CTA } from "@/sections";
+import React from "react";
 
 const armada = () => {
   return (
     <Layout>
-      <div>
-        <ul>
-          <li>
-            <Link href={"/"}>Index</Link>
-          </li>
-          <li>
-            <Link href={"/armada"}>Armada</Link>
-          </li>
-          <li>
-            <Link href={"/tentang_kami"}>Tentang</Link>
-          </li>
-        </ul>
-      </div>
+      <Armada />
+      <CTA />
     </Layout>
-  )
-}
+  );
+};
 
-export default armada
+export default armada;
